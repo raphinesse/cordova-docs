bfcf86164762958e6ab999c82e6e3f32af4288b2
Raphael von der Grün <raphinesse@gmail.com>
2019-05-08 21:09:38 +0200

    Faster PR builds & use Travis CI's deploy phase
    - Always run `npm test` on during CI runs
    - Do a quick development build during `npm test`
    - Only run full production build before deployment
    - Replace `buildAndDeploy.sh` by `deploy.sh` which deploys an existing
      production build and is also usable outside of a Travis CI context
    - Unchanged: Only deploy on commits to master (not when checking PRs)

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+svn_url=https://svn.apache.org/repos/asf/cordova/site
+
+svn_with_auth() {
+    [ $svn_username ] && [ $svn_pass ] || {
+        echo 'ERROR: No SVN credentials given in $svn_username and $svn_pass'
+        exit 1
+    }
+    svn --non-interactive --no-auth-cache \
+        --username="$svn_username" --password="$svn_pass" "$@"
+}
+
+echo "Deploying website and docs to $svn_url"
+cd ..
+svn_with_auth checkout $svn_url cordova-website
+cp -R cordova-docs/build-prod/. cordova-website/public/
+cd cordova-website
+svn status | grep "?"
+svn add --force .
+svn_with_auth commit -m "Updated docs"
